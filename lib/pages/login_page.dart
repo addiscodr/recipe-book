ca696fc43656c3e11bb1774cba060c0fac59e9eb
Esayas F. Gebresilasie <addiscodr@gmail.com>
2026-04-10 12:08:04 +0300

    Modify login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,32 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Login", style: TextStyle(fontSize: 24)),
+      ),
+      body: _buildUI(context),
+
+      //
+    );
+  }
+
+  Widget _buildUI(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [_title(context)],
+    );
+  }
+
+  Widget _title(BuildContext context) {
+    return const Row(
+      children: [
+        Text("Recipe", style: TextStyle(fontSize: 36)),
+        Text("Book", style: TextStyle(fontSize: 36, color: Colors.deepOrange)),
+      ],
+    );
   }
 }
